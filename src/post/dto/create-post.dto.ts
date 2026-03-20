9ab13e07896ee3d012fea5ce8997268fd18eac97
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-03-20 12:44:57 -0300

    feat: crear DTO

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePostDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { ImageEntity } from "../entities/image.entity";
+import { IsString } from "class-validator";
+
+export class CreatePostDto {
+    @ApiProperty()
+    @IsString()
+    title: string;
+
+    @ApiProperty()
+    @IsString()
+    slug: string
+
+    @ApiProperty()
+    @IsString()
+    content: string;
+    
+    @ApiProperty({ required: false })
+    @IsString({ each: true })
+    images: [ImageEntity];
+
+    @ApiProperty()
+    @IsString()
+    authorId: number;
+
+}
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
+import { ImageEntity } from '../entities/image.entity';
+import { IsString } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+
+    @ApiProperty({ required: false })
+    @IsString()
+    title?: string | undefined;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    slug?: string | undefined;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    content?: string | undefined;
+
+    @ApiProperty({ required: false })
+    @IsString({ each: true })
+    images?: [ImageEntity];
+}
