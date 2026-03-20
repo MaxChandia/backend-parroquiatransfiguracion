3a8653e159589aadd6ce5ba8e344f514e8f908fb
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-03-20 12:46:06 -0300

    feat: inserción de exceptionfilter en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { PrismaExceptionFilter } from './prisma/prisma-exception.filter';
 
 
 
@@ -8,6 +9,7 @@ import { setupSwagger } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new PrismaExceptionFilter());
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
