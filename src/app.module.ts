48f2c1885d2469d6d478d89261d0a748b78c54d5
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-03-25 01:08:14 -0300

    feat: package modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { AwsModule } from './aws/aws.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, PostModule, AuthModule],
+  imports: [UserModule, PrismaModule, PostModule, AuthModule, AwsModule],
   controllers: [AppController],
   providers: [AppService],
 })
