25b287499766beefcd18b2a1d7d12803d76a3ee8
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-04-03 12:01:13 -0300

    feat: agregar CORS

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new PrismaExceptionFilter());
+  app.enableCors(
+    {origin:['http://localhost:3000', 'https://www.parroquiatransfiguracion.cl/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+  }
+  );
   setupSwagger(app); 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
